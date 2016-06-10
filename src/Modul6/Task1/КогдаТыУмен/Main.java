41725e9f3049493bef83a9bd1f39d90ca6fae000
nikitarozhkov <egorrozhkov@bk.ru>
2016-06-10 15:31:25 +0300

    1

---
+++ src/Modul6/Task1/КогдаТыУмен/Main.java
@@ -0,0 +1,14 @@
+package Modul6.Task1.КогдаТыУмен;
+
+/**
+ * Created by nikitarozhkov on 10.06.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Test();
+
+    }
+    public static void Test(){
+        throw new OutOfMemoryError("не знаю зачем");
+    }
+}
